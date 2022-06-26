2b9778f2fdceffcef157a2cdcbf850e4a6420c67
Jan Quest <jan@questnet.eu>
2022-06-26 13:16:28 +0200

    adding babel for multi language support

--- app/app.py
+++ app/app.py
@@ -4,13 +4,13 @@ from resource.customer import Customer
 from flask import Flask
 from flask_babel import Babel
 from flask_restful import Api
-from omegaconf import DictConfig
 
 from db import db
 from ma import ma
 
 
 def build_app():
+    """Create the flask app and configure ist using classes from 'config.py'."""
     app = Flask(__name__)
     app.config.from_object("config.Config")
     app.config.from_object("config.ConfigSQL")
@@ -19,6 +19,7 @@ def build_app():
 
     @app.before_first_request
     def create_tables():
+        """Create tables before the first request runs, if they do not already exist."""
         print("Creating tables")
         db.create_all()
 
@@ -26,23 +27,28 @@ def build_app():
 
 
 def build_babel(app):
+    """Initialize Babel."""
     babel = Babel(app)
     return babel
 
 
 def build_api(app):
+    """Initialize flask_restful Api and add resources."""
     api = Api(app)
     api.add_resource(Customer, "/customer/<string:customer_number>")
     return api
 
 
 def build_app_routes(app):
+    """Create routes for non-API pages."""
+
     @app.route("/")
     def home() -> str:
         return "QuestNet API"
 
 
 def main() -> None:
+    """Initialize the app and all dependencies. Finally run it."""
     app = build_app()
     db.init_app(app)
     ma.init_app(app)
@@ -51,7 +57,8 @@ def main() -> None:
     build_api(app)
 
     app.run(
-        debug=os.getenv("DEBUG", default=False), port=os.getenv("PORT", default=5000)
+        debug=os.getenv("DEBUG", default=False),  # type: ignore
+        port=os.getenv("PORT", default=5000),  # type: ignore
     )
 
 
--- app/config.py
+++ app/config.py
@@ -8,23 +8,31 @@ load_dotenv(path.join(basedir, ".env"))
 
 
 class Config:
+    """General config settings for flask."""
+
     DEBUG = getenv("DEBUG")
     PORT = getenv("PORT")
     SECRET_KEY = getenv("SECRET_KEY")
 
 
 class ConfigSQL:
+    """Config settings for SQLAlchemy."""
+
     SQLALCHEMY_DATABASE_URI = getenv("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PROPAGATE_EXCEPTIONS = True
 
 
 class ConfigJWT:
+    """Config settings for JWT."""
+
     JWT_SECRET_KEY = getenv("JWT_SECRET_KEY")
     JWT_BLACKLIST_ENABLED = True
     JWT_BLACKLIST_TOKEN_ACCESS = ["access", "refresh"]
 
 
 class ConfigBabel:
+    """Config settings for Babel."""
+
     BABEL_DEFUALT_LOCALE = getenv("BABEL_DEFUALT_LOCALE")
     BABEL_DEFAULT_TIMEZONE = getenv("BABEL_DEFAULT_TIMEZONE")
