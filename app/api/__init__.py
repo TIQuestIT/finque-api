a7fe308263bcd1a4700f4f3a8f57faf6c096ecf7
Jan Quest <jan@questnet.eu>
2022-07-02 21:33:13 +0200

    add api namespaces

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,9 +1,12 @@
 from flask_restx import Api
 
-from api.resources.customer import api as customer
-from api.resources.home import api as home
+from api.resources.customer import api_customer
+from api.resources.home import api_home
+from api.resources.user import api_role, api_user
 
 api = Api(title="FinQue API", version="0.1", description="API for FinQue Project")
 
-api.add_namespace(home, path="/")
-api.add_namespace(customer, path="/customer")
+api.add_namespace(api_home, path="/")
+api.add_namespace(api_user, path="/user")
+api.add_namespace(api_role, path="/role")
+api.add_namespace(api_customer, path="/customer")
