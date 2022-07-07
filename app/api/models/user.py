de2f48cbd70f87ee20890a8ce830b3f8ad14aa87
Jan Quest <jan@questnet.eu>
2022-07-07 21:36:33 +0200

    update user model to use flask-security

--- app/api/models/user.py
+++ app/api/models/user.py
@@ -1,8 +1,8 @@
-from typing import List
 from uuid import uuid4
 
 from core.db import db
 from flask_security.core import RoleMixin, UserMixin
+from flask_security.datastore import SQLAlchemyUserDatastore
 
 
 def _get_uuid():
@@ -11,67 +11,28 @@ def _get_uuid():
 
 class RolesUsersModel(db.Model):
     __tablename__ = "roles_users"
-    id = db.Column(db.String(), primary_key=True, default=_get_uuid)
-    user_id = db.Column("user_id", db.String(), db.ForeignKey("user.id"))
-    role_id = db.Column("role_id", db.String(), db.ForeignKey("role.id"))
-
-    @classmethod
-    def find_by_id(cls, roles_users_id: str) -> "RolesUsersModel":
-        return cls.query.filter_by(id=roles_users_id).first()
-
-    @classmethod
-    def find_by_user_id(cls, user_id: str) -> "RolesUsersModel":
-        return cls.query.filter_by(user_id=user_id).first()
-
-    @classmethod
-    def find_by_role_id(cls, role_id: str) -> "RolesUsersModel":
-        return cls.query.filter_by(role_id=role_id).first()
-
-    @classmethod
-    def find_all(cls) -> List["RolesUsersModel"]:
-        return cls.query.all()
-
-    def save_to_db(self) -> None:
-        roles = self.roles
-        UserModel.roles.append(roles)
-        db.session.add(self)
-        db.session.commit()
-
-    def delete_from_db(self) -> None:
-        db.session.delete(self)
-        db.session.commit()
+    user_id = db.Column(
+        "user_id", db.Integer, db.ForeignKey("user.id"), primary_key=True
+    )
+    role_id = db.Column(
+        "role_id", db.Integer, db.ForeignKey("role.id"), primary_key=True
+    )
 
 
 class RoleModel(db.Model, RoleMixin):
     __tablename__ = "role"
-    id = db.Column(db.String(), primary_key=True, default=_get_uuid)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True)
     description = db.Column(db.String(255))
 
     @classmethod
-    def find_by_role_id(cls, role_id: str) -> "RoleModel":
-        return cls.query.filter_by(id=role_id).first()
-
-    @classmethod
-    def find_by_role_name(cls, name: str) -> "RoleModel":
-        return cls.query.filter_by(name=name).first()
-
-    @classmethod
-    def find_all(cls) -> List["RoleModel"]:
+    def find_all(cls):
         return cls.query.all()
-
-    def save_to_db(self) -> None:
-        db.session.add(self)
-        db.session.commit()
-
-    def delete_from_db(self) -> None:
-        db.session.delete(self)
-        db.session.commit()
 
 
 class UserModel(db.Model, UserMixin):
     __tablename__ = "user"
-    id = db.Column(db.String, primary_key=True, default=_get_uuid)
+    id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(255), unique=True)
     username = db.Column(db.String(255), unique=True, nullable=True)
     password = db.Column(db.String(255), nullable=False)
@@ -90,21 +51,8 @@ class UserModel(db.Model, UserMixin):
     )
 
     @classmethod
-    def find_by_user_id(cls, user_id: str) -> "UserModel":
-        return cls.query.filter_by(id=user_id).first()
-
-    @classmethod
-    def find_by_username(cls, username: str) -> "UserModel":
-        return cls.query.filter_by(username=username).first()
-
-    @classmethod
-    def find_all(cls) -> List["UserModel"]:
+    def find_all(cls):
         return cls.query.all()
 
-    def save_to_db(self) -> None:
-        db.session.add(self)
-        db.session.commit()
 
-    def delete_from_db(self) -> None:
-        db.session.delete(self)
-        db.session.commit()
+user_datastore = SQLAlchemyUserDatastore(db, UserModel, RoleModel)
